48c68e5f94d4db7bea3c13b1c482d44885da7dcf
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-12 17:09:38 -0300

    criei ex de IMC com variáveis

---
+++ aula8/index.js
@@ -0,0 +1,10 @@
+const nome = 'Henrique Almeida Soares'
+const idade = 18
+const peso = 70
+const altura = 1.70
+let anoNascimento
+let imc = peso / (altura * altura)
+anoNascimento = 2023 - idade
+console.log(`${nome} tem ${idade} anos e pesa ${peso} kg`)
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`)
+console.log(`${nome} nasceu em ${anoNascimento}`)
