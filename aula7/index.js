1c649d9fd739ee15d25f65533e986264b91ea414
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-12 11:33:43 -0300

    atualizei aula7

---
+++ aula7/index.js
@@ -0,0 +1,24 @@
+//Não podemos criar constantes com palavras reservadas
+//Constantes precisam ter nomes significativos
+//Não pode começar o nome de uma constante com um número
+//Não podem conter espaços ou traços
+//Utilizamos camelCase
+//Case-sensitive
+//Não pode modificar o valor de uma constante
+//NÃO UTILIZE VAR, UTILIZE CONST
+
+const nome = 'vambas'
+console.log(nome)
+
+const n1 = 5
+console.log(n1)
+const n2 = 5
+console.log(n2)
+const soma = n1 * n2
+console.log(soma)
+const somaDuplicada = soma * 2
+console.log(somaDuplicada)
+let somaTriplicada = soma * 3
+console.log(somaTriplicada)
+somaTriplicada = somaTriplicada + 5
+console.log(somaTriplicada)
