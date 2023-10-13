d95822a3d31c5436eaf54befa227cf154d4bd194
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-13 09:03:03 -0300

    criei aula10

---
+++ aula10/index.js
@@ -0,0 +1,14 @@
+// Tipos Primitivos de Dados
+// string, number, undefined, null, boolean
+
+
+const nome1 = 'Henrique' // String
+const nome2 = "Henrique" // String
+const nome3 = `Henrique` // String
+const n1 = 120 // Number
+const n2 = 30.45 // Number
+let contaAluno // Undefined = Não aponta pra nenhum local da memória
+const contaAluno2 = null // Nulo = Não aponta pra nenhum local da memória
+const aprovado = false // Boolean = true, false (valor lógico)
+
+console.log(typeof aprovado)
