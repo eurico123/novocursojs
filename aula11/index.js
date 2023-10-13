92a737fee8d553af03bf7ba0d042058dbb601011
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-13 12:09:13 -0300

    Criei aula de operadores

---
+++ aula11/index.js
@@ -0,0 +1,33 @@
+/* 
+    ARITMÉTICOS
+    + = Adição / Concatenação
+    - = Subtração
+    * = Multiplicação
+    / = Divisão
+    ** = Potenciação
+    % = Resto da Divisão
+
+*/
+
+/*
+    ORDEM DE PRECEDÊNCIA
+    1 = ()
+    2 = **
+    3 = * / %
+    4 = + -
+
+*/
+
+/*
+    Incremento = ++
+    Decremento = --
+
+*/
+
+// NaN = Not a Number
+// parseInt = (inteiro)
+// parseFloat = (decimais)
+// Number = (O JS coverte no automático)
+const num1 = 12
+const num2 = Number('6.5')
+console.log(num1 + num2)
