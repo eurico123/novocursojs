805ef12c0386a61c7b2660bd95fe407febfddd4e
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-16 13:59:21 -0300

    aula sobre strings

---
+++ aula13/aula13.js
@@ -0,0 +1,11 @@
+
+//              '01234567'
+let umaString = 'Um texto'
+
+console.log(umaString.replace('Um', 'Outro'))
+
+/*
+console.log(umaString.concat(' em um lindo dia.'))
+console.log(umaString + ' em um lindo dia.')
+console.log(`${umaString} em um lindo dia.`)
+*/
