581e83057711f720e5d0a85c4ecf03a2f819ff8d
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-16 09:26:11 -0300

    criei ex de prompt com js

---
+++ aula11.2/js/script.js
@@ -0,0 +1,6 @@
+let n1 = window.prompt('Digite um número:')
+n1 = Number(n1)
+let n2 = window.prompt('Digite outro número:')
+n2 = Number(n2)
+const res = n1 + n2
+window.alert(`O resultado da soma entre os dois números foi de ${res}`)
