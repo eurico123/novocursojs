1f6e3bc6c9feaf5d1f0137dfebf78361b050565b
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-18 13:40:19 -0300

    criei aula15 sobre objeto Math

---
+++ aula15/index.js
@@ -0,0 +1,12 @@
+let num1 = 9.487982
+
+
+
+//let num2 = Math.floor(num1) floor = arredonda o número pra baixo
+//let num2 = Math.ceil(num1) ceil = arredonda o número pra cima
+//let num2 = Math.round(num1) round = arredonda pro número mais próximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)) max = maior número da sequência
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)) min = menor número da sequência
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5) random = número aleatório 
+//console.log(Math.PI) PI = valor de PI
+//console.log(Math.pow(2, 10)) pow = faz a potenciação entre números
