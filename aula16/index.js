b7695604160dd331d2950e42c298c8fc97d77e41
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-20 16:48:12 -0300

    criei aula16 sobre arrays

---
+++ aula16/index.js
@@ -0,0 +1,42 @@
+
+
+//               0        1           2
+let alunos = ['Maria', 'Leonardo', 'João']
+alunos = 1, 2, 3
+
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+// alunos.push('Luíza') = Adiciona elementos no final da Array
+// alunos.push('Eduardo')
+
+// console.log(alunos.slice(0, 5)) = Corta elementos de uma Array
+
+// console.log(alunos[50]) = Mostra uma Array que não existe
+
+// delete alunos[1] = Deleta um elemento de uma Array
+// console.log(alunos)
+
+
+// const removido = alunos.shift() = Remove elemento do coemço de uma Array
+// console.log(removido)
+// console.log(alunos)
+
+
+// alunos.unshift('Gabriel') = Adiciona no começo
+// alunos.unshift('Vitor')
+
+// alunos.push('Gabriel'); = Adiciona no fim
+// alunos.push('Alessandro');
+
+// alunos[alunos.length] = 'Fábio' = Adiciona no fim
+// alunos[alunos.length] = 'Gabriel'
+// alunos[alunos.length] = 'Vitor'
+
+// alunos[2] = 'Sérgio' = Altera
+// alunos[3] = 'Sacani' = Adicionando
+
+// console.log(alunos) = Acessando
+// console.log(alunos[0])
+// console.log(alunos[2])
