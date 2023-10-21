74d584210267820cc1a5c27277fca6e2a5e1f6ab
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-20 22:55:08 -0300

    criei aula17 sobre functions

---
+++ aula17/index.js
@@ -0,0 +1,25 @@
+/*
+function soma(x = 1, y = 1){
+    const resultado = x + y
+    return resultado
+}
+
+const resultado = soma(6, 2)
+console.log(resultado)
+*/
+
+/*
+const raiz = function(n){
+    return n ** 0.5
+};
+
+console.log(raiz(36))
+console.log(raiz(25))
+console.log(raiz(16))
+*/
+
+const raiz = n => n ** 0.5
+
+console.log(raiz(36))
+console.log(raiz(25))
+console.log(raiz(16))
