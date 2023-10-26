9b3fc3e39f4d2aafe44cb7e8b13f03fdfef2cd25
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-26 10:49:41 -0300

    atualizei aula22 sobre operadores lógicos

--- aula22/index.js
+++ aula22/index.js
@@ -3,7 +3,18 @@
 Operadores Lógicos
 -------------------
 && -> AND -> E
-| -> OR -> OU
+|| -> OR -> OU
 ! -> NOT -> NÃO
 
 */
+
+//const expressaoAnd = true && true && true && true
+//const expressaoOr = false || false || false || false
+
+//const usuario = 'Henrique' // form usuário digitou
+//const senha = '123456' // form usuário digitou
+
+//                      true                    false
+//const vaiLogar = usuario === 'Henrique' && senha === '12345'
+
+console.log(!!true)
