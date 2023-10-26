3ed2da7a8d01ded52c266170c54ba56275cebc8b
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-25 21:48:41 -0300

    criei aula21 com operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,18 @@
+/*
+
+Operadores de Comparação
+--------------------------
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade
+=== igualdade estrita
+!= diferente
+!== diferente estrito
+
+*/
+const num1 = 10 //number
+const num2 = 10 //number
+const comp = num1 !== num2
+console.log(comp)
