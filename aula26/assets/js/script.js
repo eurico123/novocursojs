5be044a53388ce16a5dc0fab2669f2ae8801e3c8
Henrique Almeida Soares <hg515900@gmail.com>
2023-10-27 11:46:07 -0300

    atualizei ex de IMC da aula26

--- aula26/assets/js/script.js
+++ aula26/assets/js/script.js
@@ -1,11 +1,66 @@
-function calcular(){
-    const peso = document.getElementById('ipeso')
-    const altura = document.getElementById('ialtura')
-    let imc = peso / (altura*2)
+const form = document.getElementById('form')
 
-    if (imc < 18,5) {
-        alert(`Seu IMC é de ${imc}, e está abaixo do peso`)
-    } else if (imc >= 18,5 && imc < 25) {
-        alert(`Seu IMC é de ${imc}, e está no peso normal`)
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const iPeso = document.getElementById('ipeso')
+    const iAltura = document.getElementById('ialtura')
+
+    const peso = Number(iPeso.value)
+    const altura = Number(iAltura.value)
+
+    if (!peso) {
+        setResultado('Peso Inválido!', false)
+        return
     }
+
+    if (!altura) {
+        setResultado('Altura Inválida!', false)
+        return
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+    
+});
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+}
+
+function criaP() {
+    const p = document.createElement('p')
+    return p 
+}
+
+function setResultado (msg, isValid) {
+    const res = document.getElementById('res')
+    res.innerHTML = ''
+    
+    const p = criaP()
+
+    if (isValid) {
+        p.classList.add('p-res');
+    } else {
+        p.classList.add('p-bad')
+    }
+
+    p.innerHTML = msg
+    res.appendChild(p)
 }
