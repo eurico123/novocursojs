2b05130546f034674e3e2b6bca5445812b41c4ee
Henrique Almeida Soares <hg515900@gmail.com>
2023-11-15 20:02:51 -0300

    aula34 sobre estrutura de repetição for

---
+++ aula34/index.js
@@ -0,0 +1,13 @@
+//console.log('linha 0');
+//console.log('linha 1');
+//console.log('linha 2');
+//console.log('linha 3');
+//console.log('linha 4');
+//console.log('linha 5');
+
+const frutas = ['maçã', 'pêra', 'uva', 'abacate', 'morango', 'cereja', 'mamão']
+//console.log(frutas.length)
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i])
+}
