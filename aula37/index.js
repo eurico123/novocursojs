4526e2bd83bb3221cc2a668fef45ad69738778f7
Henrique Almeida Soares <hg515900@gmail.com>
2023-11-16 15:30:37 -0300

    aula37 sobre for of

---
+++ aula37/index.js
@@ -0,0 +1,31 @@
+const pessoa = {
+    nome: 'Henrique',
+    sobrenome: 'Almeida',
+    idade: 18
+};
+
+// For clássico --> Geralmente com iteráveis (arrays ou strings)
+// For In --> Retorna o índice ou chave (strings, arrays ou objetos)
+// For Of --> Retorna o valor em si (iteráveis, arrays ou strings)
+
+//for (let i = 0; i < nomes.length; i++) {
+//    console.log(nomes[i])
+//}
+
+//console.log('#####')
+
+//for (let i in nomes) {
+//    console.log(nomes[i])
+//}
+
+//console.log('#####')
+
+//for (let valor of nomes) {
+//    console.log(valor)
+//}
+
+//console.log('#####')
+
+//nomes.forEach(function (valor, indice, array) {
+//    console.log(valor, indice, array)
+//})
