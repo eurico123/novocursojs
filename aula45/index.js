6d448097977a4b018bf7b8c9fe2f2d57ead31f4a
Henrique Almeida Soares <hg515900@gmail.com>
2023-11-21 15:01:02 -0300

    aula45 sobre try, catch e finally

---
+++ aula45/index.js
@@ -0,0 +1,24 @@
+function retornaHora (data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date');
+    };
+
+    if (!data) {
+        data = new Date();
+    };
+
+    return data.toLocaleTimeString('pt-BR', {
+
+    });
+}
+
+try {
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora()
+    console.log(hora)
+} catch (e) {
+    // tratar erro
+    // console.log(e)
+} finally {
+    console.log('Tenha um bom dia.')
+}
