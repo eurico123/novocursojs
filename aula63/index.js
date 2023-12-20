3453710a204176d13a1397894e9d30160d894f75
Henrique Almeida Soares <hg515900@gmail.com>
2023-12-20 14:34:50 -0300

    aula63 sobre método splice

---
+++ aula63/index.js
@@ -0,0 +1,15 @@
+//               -5       -4       -3         -2        -1
+//                0        1        2          3         4
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// Unshift
+nomes.splice(0, 0, 'Henrique')
+console.log(nomes);
+
+
+// nomes.splice(índice, delete, elem1, elem2, elem3)
+//pop
+//const removido = nomes.splice(-1, 1);
+
+//shift
+//const removido = nomes.splice(0, 1)
