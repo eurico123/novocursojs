4d07d40112697d1a6595eb624b204bf25dc0e9f8
Henrique Almeida Soares <hg515900@gmail.com>
2023-12-20 14:02:54 -0300

    aula62 revisão de arrays

---
+++ aula62/index.js
@@ -0,0 +1,12 @@
+// Valor por Referência
+//                  0         1        2       3          4
+//const nomes = ['Henrique', 'Maria', 'João', 'Joana', 'Guilherme'];
+//const novo = nomes.slice(1, 4);
+//console.log(novo);
+
+
+//const nome = 'Henrique Almeida Soares';
+const nomes = [ 'Henrique', 'Almeida', 'Soares' ];
+const nome = nomes.join(' ');
+
+console.log(nome);
